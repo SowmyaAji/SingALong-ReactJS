7978dffa2a4f88e6398b86e1c1f9b0b24c45a4aa
Sowmya <sowmya.aji@gmail.com>
2019-05-03 18:33:11 -0400

    Add a line to index.js

--- src/index.js
+++ src/index.js
@@ -30,20 +30,16 @@ class App extends Component {
         return <div>Sorry something went wrong! Please try something else</div>;
       } else {
         this.setState({ songs: data.results });
-        console.log(data.results)
       }
     });
   }
 
   selectSong = (song) => {
     this.setState({ playSong: song });
-    console.log(song)
   }
 
   hearSong = () => {
     if (this.state.playSong) {
-      console.log("new song");
-      console.log(this.state.playSong);
       return (<div>
         <ReactAudioPlayer
           src={this.state.playSong.previewUrl}
@@ -53,28 +49,23 @@ class App extends Component {
       </div>
       );
     }
-
   }
-
-
   render() {
     const songs = this.state.songs.map(song =>
       <div onClick={this.selectSong.bind(this, song)} style={{ width: '250px', height: '350px', margin: '10px', background: '#F78DA7', textAlign: "center", display: "inline-block", border: "1px solid black" }}><p>Song: {song.trackName}</p>
         <img src={song.artworkUrl100} alt="Album cover" />
         <p>Artist: {song.artistName}</p>
         <p>Album: {song.collectionName}</p>
-
       </div>);
-
     return (
-
 
       <div style={{ textAlign: "center" }}>
         <h1 style={{ textAlign: "center", background: "#bf4d69", color: "white" }}>Sing-a-long</h1>
         <input style={{ width: "400px", height: "80px", borderRadius: "20px", border: "1px solid black", fontSize: " 24px", textAlign: "center" }} type="text" onChange={this.searchHandler} placeholder="Pick a song, singer or album"
         />
+        <p>Click on any to hear a short bit!</p>
+
         <div>
-          <p>Click on any to hear a short bit of it!</p>
           {this.hearSong()}
           {songs}
         </div>
