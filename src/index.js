57370a3080332a0456efe9f8e7f5cc367a3d2593
Sowmya <sowmya.aji@gmail.com>
2019-05-03 17:57:43 -0400

    CSS changes to input box

--- src/index.js
+++ src/index.js
@@ -12,69 +12,70 @@ class App extends Component {
       songs: [],
       name: 'React',
       playSong: null
-         
+
     };
   }
 
-  searchHandler = (evt) => { 
+  searchHandler = (evt) => {
     this.searchQuery(evt.target.value);
-    
+
   }
 
-  searchQuery= (query) =>   {
-    const queryUrl = 'https://itunes.apple.com/search?media=music&limit=20&term=' + query;  
-  jsonp(queryUrl, null, (err, data) => {
+  searchQuery = (query) => {
+    const queryUrl = 'https://itunes.apple.com/search?media=music&limit=20&term=' + query;
+    jsonp(queryUrl, null, (err, data) => {
       if (err) {
         console.error(err.message);
       } else {
-        this.setState({songs: data.results});
-        console.log(data.results)   }
+        this.setState({ songs: data.results });
+        console.log(data.results)
+      }
     });
   }
 
-selectSong =(song) => {
-  this.setState({playSong: song});
-  console.log(song)
-}
+  selectSong = (song) => {
+    this.setState({ playSong: song });
+    console.log(song)
+  }
 
-hearSong = () => {
-  if (this.state.playSong) {
+  hearSong = () => {
+    if (this.state.playSong) {
       console.log("new song");
       console.log(this.state.playSong);
-      return ( <div>
-                <ReactAudioPlayer
-                  src={this.state.playSong.previewUrl}
-                  autoPlay
-                  controls
-                />
-          </div>
+      return (<div>
+        <ReactAudioPlayer
+          src={this.state.playSong.previewUrl}
+          autoPlay
+          controls
+        />
+      </div>
       );
     }
-    
-}
- 
+
+  }
+
 
   render() {
-    const songs = this.state.songs.map(song => 
-    <div onClick={this.selectSong.bind(this,song)} style={{ width: '200px', height: '300px', margin: '10px', background: '#F78DA7', textAlign: "center", display: "inline-block", border: "1px solid black" }}><p>Song: {song.trackName}</p>
-          <img src={song.artworkUrl100}/>
-          <p>Artist: {song.artistName}</p>
-          <p>Album: {song.collectionName}</p>
-    
-    </div>);
+    const songs = this.state.songs.map(song =>
+      <div onClick={this.selectSong.bind(this, song)} style={{ width: '200px', height: '300px', margin: '10px', background: '#F78DA7', textAlign: "center", display: "inline-block", border: "1px solid black" }}><p>Song: {song.trackName}</p>
+        <img src={song.artworkUrl100} />
+        <p>Artist: {song.artistName}</p>
+        <p>Album: {song.collectionName}</p>
+
+      </div>);
 
     return (
-      
-     
-      <div>
-      <h1 style={{ textAlign: "center", background: "#bf4d69", color: "white" }}>Sing-a-long</h1>
-       <input type="text" onChange={this.searchHandler}
+
+
+      <div style={{ textAlign: "center" }}>
+        <h1 style={{ textAlign: "center", background: "#bf4d69", color: "white" }}>Sing-a-long</h1>
+        <input style={{ width: "200px", height: "30px" }} type="text" onChange={this.searchHandler} placeholder="Pick a song, singer or album"
         />
         <div>
-        {this.hearSong()}
-       {songs}
-       </div>
-      
+          {this.hearSong()}
+          {songs}
+        </div>
+
       </div>
     );
   }
