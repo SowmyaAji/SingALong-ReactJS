f26a719f2e9f0e4ea7683dee955c7dfa191ecdf1
Sowmya <sowmya.aji@gmail.com>
2019-05-03 17:50:58 -0400

    Updates

---
+++ precache-manifest.ac785337d696015607a3d4db6286eb97.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0340edfc408482c25042bc8c0e44a04f",
+    "url": "/SingALong-ReactJS/index.html"
+  },
+  {
+    "revision": "28434fafbc4447862b29",
+    "url": "/SingALong-ReactJS/static/css/main.170f56e8.chunk.css"
+  },
+  {
+    "revision": "4138017aa1a02dcac393",
+    "url": "/SingALong-ReactJS/static/js/2.9e81544c.chunk.js"
+  },
+  {
+    "revision": "28434fafbc4447862b29",
+    "url": "/SingALong-ReactJS/static/js/main.be093df3.chunk.js"
+  },
+  {
+    "revision": "859bf199e2bf5d665a2f",
+    "url": "/SingALong-ReactJS/static/js/runtime~main.b5c7caf3.js"
+  }
+]);
